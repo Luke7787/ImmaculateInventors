50127c6da6ffef520af5c3d9bd6b65b2c3c4082f
Adam Ta <adamta2004@gmail.com>
2024-02-07 15:40:23 -0800

    cleared front end

---
+++ src/MyApp.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyApp = () => {
+  return (
+    <div>
+      <h1>My React TypeScript App</h1>
+    </div>
+  );
+};
+
+export default MyApp;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MyApp from './MyApp';
+import MyApp from './MyApp.tsx';
 import './index.css';
 
 ReactDOM.render(<MyApp />, document.getElementById('root'));
