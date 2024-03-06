6f8f90c5f8468741f625614b3573050dc730d1eb
Adam Ta <adamta2004@gmail.com>
2024-03-05 23:13:22 -0800

    validation

--- src/LeftOptionNav/LeftOptionNav.tsx
+++ src/LeftOptionNav/LeftOptionNav.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import styles from "./LeftOptionNav.module.css";
+import React from 'react';
+import styles from './LeftOptionNav.module.css';
 
 interface LeftOptionNavProps {
-  numRows: number;
-  fieldNames: string[];
+	numRows: number;
+	fieldNames: string[];
 }
 
 const LeftOptionNav = ({ numRows, fieldNames }: LeftOptionNavProps) => {
-  return (
-    <div className={styles.rowName}>
-      Hello
-    </div>
-  );
+	return (
+		<div className={styles.rowName}>
+			Hello {numRows} {fieldNames}
+		</div>
+	);
 };
 
 export default LeftOptionNav;
