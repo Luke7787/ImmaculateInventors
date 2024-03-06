2bcf16ea37c32ed2b76bc42e304b33bbfe84d23a
Adam Ta <adamta2004@gmail.com>
2024-03-06 15:41:40 -0800

    changed schema

--- express-backend/models/user.tsx
+++ express-backend/models/user.tsx
@@ -1,31 +1,63 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    validate: {
-      validator: function(value){
-        const passwordPolicy = /^(?=.*\d)(?=.*[?!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{10,}$/;
-        return passwordPolicy.test(value);
-      },
-      message: props => 'Error: Password must be 10 characters long, have a special character, uppercase character, and a number'
-    }
-    
-    
-    // validate(value) {
-    //   // if (value.length < 5) throw new Error("Error: Password must be 5 or more characters long.");
-    //   if (value.length < 5) res.status(409).send("Error: Password must be 5 or more characters long.");
-    // },
-  },
-  
-}, {collection : 'users_list'});
+const UserSchema = new mongoose.Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		country: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		state: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		city: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		zipcode: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		username: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			trim: true,
+			validate: {
+				validator: function (value) {
+					const passwordPolicy =
+						/^(?=.*\d)(?=.*[?!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{10,}$/;
+					return passwordPolicy.test(value);
+				},
+				message: (props) =>
+					'Error: Password must be 10 characters long, have a special character, uppercase character, and a number',
+			},
+		},
+	},
+	{ collection: 'users_list' }
+);
 
 module.exports = UserSchema;
-
