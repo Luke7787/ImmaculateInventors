71caf49cc2d1f0cdf979b87e85331759239e7640
Adam Ta <adamta2004@gmail.com>
2024-03-17 10:38:29 -0700

    demo test case

--- express-backend/models/user-services.test.tsx
+++ express-backend/models/user-services.test.tsx
@@ -1,10 +1,28 @@
 const userServices = require('./user-services.tsx');
 const mongoose = require('mongoose');
-import '@types/jest';
+import 'jest';
 
 afterAll(() => mongoose.disconnect());
-test('getUsers', async () => {
-	expect(await userServices.getUsers()).toBe(1);
+
+jest.mock('mongoose');
+describe('getUsers', () => {
+	it('returns all users from database', async () => {
+		const mockUserModel = {
+			find: jest.fn().mockResolvedValue([
+				{ username: 'user1', password: 'password1' },
+				{ username: 'user2', password: 'password2' },
+			]),
+		};
+		mongoose.createConnection.mockReturnValueOnce({
+			model: jest.fn().mockReturnValueOnce(mockUserModel),
+		});
+		const result = await userServices.getUsers();
+		expect(mockUserModel.find).toHaveBeenCalledTimes(1);
+		expect(result).toEqual([
+			{ username: 'user1', password: 'password1' },
+			{ username: 'user2', password: 'password2' },
+		]);
+	});
 });
 
 export {};
