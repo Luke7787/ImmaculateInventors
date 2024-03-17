aa6216ac9d83803a46803ec82adb4c922816b3c2
Adam Ta <adamta2004@gmail.com>
2024-03-17 10:55:15 -0700

    updated tests

--- express-backend/models/user-services.test.tsx
+++ express-backend/models/user-services.test.tsx
@@ -2,26 +2,28 @@ const userServices = require('./user-services.tsx');
 const mongoose = require('mongoose');
 import 'jest';
 
-afterAll(() => mongoose.disconnect());
-
 jest.mock('mongoose');
-describe('getUsers', () => {
-	it('returns all users from database', async () => {
-		const mockUserModel = {
-			find: jest.fn().mockResolvedValue([
+describe('User services', () => {
+	afterAll(() => mongoose.disconnect());
+	describe('getUsers', () => {
+		it('returns all users from database', async () => {
+			const mockUserModel = {
+				find: jest.fn().mockResolvedValue([
+					{ username: 'user1', password: 'password1' },
+					{ username: 'user2', password: 'password2' },
+				]),
+			};
+			mongoose.createConnection.mockReturnValueOnce({
+				model: jest.fn().mockReturnValueOnce(mockUserModel),
+			});
+			const result = await userServices.getUsers();
+			expect(mockUserModel.find).toHaveBeenCalledTimes(1);
+			expect(result).toEqual([
 				{ username: 'user1', password: 'password1' },
 				{ username: 'user2', password: 'password2' },
-			]),
-		};
-		mongoose.createConnection.mockReturnValueOnce({
-			model: jest.fn().mockReturnValueOnce(mockUserModel),
+			]);
 		});
-		const result = await userServices.getUsers();
-		expect(mockUserModel.find).toHaveBeenCalledTimes(1);
-		expect(result).toEqual([
-			{ username: 'user1', password: 'password1' },
-			{ username: 'user2', password: 'password2' },
-		]);
+		// TODO: test when a user/pass is given
 	});
 });
 
