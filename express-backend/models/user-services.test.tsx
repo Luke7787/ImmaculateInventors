3736bcfc8f2b4424085b0fc88df23635da5d05c2
AnsonWooo <ansonwu020603@gmail.com>
2024-03-17 18:03:53 -0700

    id

--- express-backend/models/user-services.test.tsx
+++ express-backend/models/user-services.test.tsx
@@ -2,29 +2,10 @@ const userServices = require('./user-services.tsx');
 const mongoose = require('mongoose');
 import 'jest';
 
-jest.mock('mongoose');
-describe('User services', () => {
-	afterAll(() => mongoose.disconnect());
-	describe('getUsers', () => {
-		it('returns all users from database', async () => {
-			const mockUserModel = {
-				find: jest.fn().mockResolvedValue([
-					{ username: 'user1', password: 'password1' },
-					{ username: 'user2', password: 'password2' },
-				]),
-			};
-			mongoose.createConnection.mockReturnValueOnce({
-				model: jest.fn().mockReturnValueOnce(mockUserModel),
-			});
-			const result = await userServices.getUsers();
-			expect(mockUserModel.find).toHaveBeenCalledTimes(1);
-			expect(result).toEqual([
-				{ username: 'user1', password: 'password1' },
-				{ username: 'user2', password: 'password2' },
-			]);
-		});
-		// TODO: test when a user/pass is given
-	});
+test('Testing sum with zero -- success', () => {
+	const target = 30;
+	const result = userServices.sum(30, 0);
+	expect(result).toBe(target);
 });
 
 export {};
