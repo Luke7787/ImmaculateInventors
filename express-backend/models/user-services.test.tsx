c1b04b57261920417bff90d3d04f3362f63bc048
Adam Ta <adamta2004@gmail.com>
2024-03-17 17:20:43 -0700

    added tests for getUsers

--- express-backend/models/user-services.test.tsx
+++ express-backend/models/user-services.test.tsx
@@ -1,24 +1,32 @@
-// Import necessary modules and functions
 import mongoose from 'mongoose';
-const { getUsers, getDbConnection, addUser } = require('./user-services.tsx'); // Update the path accordingly
+const { getUsers, getDbConnection, addUser } = require('./user-services.tsx');
+const UserSchema = require('./user.tsx');
 
 describe('getUsers function', () => {
 	let conn: any;
 	beforeAll(async () => {
 		conn = await getDbConnection();
-		await addUser({ username: 'dawglog', password: 'ta' }, conn);
 	});
 	afterAll(async () => {
 		await mongoose.disconnect();
 	});
 	it('all users', async () => {
 		const users = await getUsers(conn);
+		console.log(users);
 		expect(users).toBeDefined();
 		expect(users.length).toBeGreaterThan(0);
 	});
 	it('specific user', async () => {
-		const user = await getUsers(conn, 'dawglog', 'ta');
-		expect(user).toBe('Ansoazasdfn');
+		const userModel = conn.model('User', UserSchema);
+		userModel.find = jest.fn().mockResolvedValue([
+			{
+				username: 'dawglog',
+				password: 'one',
+			},
+		]);
+		const user = await getUsers(conn, 'dawglog', 'one');
+		expect(user[0].username).toBe('dawglog');
+		expect(user[0].password).toBe('one');
 	});
 });
 
