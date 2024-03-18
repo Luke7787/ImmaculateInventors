4db53bc6d6a7e7f21a86cf8fb319cc37a855973d
AnsonWooo <ansonwu020603@gmail.com>
2024-03-17 20:10:07 -0700

    testing

--- express-backend/models/user-services.tsx
+++ express-backend/models/user-services.tsx
@@ -55,7 +55,7 @@ async function addUser(user: any, conn: any) {
 		// passing the JSON content of the Document:
 		const userToAdd = new userModel(user);
 		const savedUser = await userToAdd.save();
-		return savedUser;
+		return true;
 	} catch (error: any) {
 		if (error.name == 'ValidationError') {
 			const errorMessage = error.errors.password
