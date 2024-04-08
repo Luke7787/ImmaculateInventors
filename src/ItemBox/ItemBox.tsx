5d205612ad32d264b5739127b0a333aedb92078f
Adam Ta <adamta2004@gmail.com>
2024-04-08 13:25:38 -0700

    added animations and small fixes for item box

--- src/ItemBox/ItemBox.tsx
+++ src/ItemBox/ItemBox.tsx
@@ -4,62 +4,78 @@ import AddItemModal from '../AddItemModal/AddItemModal.tsx';
 import Button from '@mui/material/Button';
 
 interface ItemProps {
-  name: string;
-  quantity: number;
-  image: string;
+	name: string;
+	quantity: number;
+	image: string;
 }
 
 interface ItemBoxProps {
-  items: ItemProps[];
-  onDelete: (name: string) => void;
-  onAddNewItem: (item: { name: string; image: string; quantity: number }) => void;
+	items: ItemProps[];
+	onDelete: (name: string) => void;
+	onAddNewItem: (item: {
+		name: string;
+		image: string;
+		quantity: number;
+	}) => void;
 }
 
 const ItemBox = ({ items, onDelete, onAddNewItem }: ItemBoxProps) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [isEditMode, setIsEditMode] = useState(false);
 
-  const splitLongName = (name) => {
-    if (name.length <= 12) return name;
-    const parts = [];
-    for (let i = 0; i < name.length; i += 12) {
-      parts.push(name.slice(i, i + 12));
-    }
-    return parts.join('\n');
-  };
-
-  return (
-    <div className={styles.outerContainer}>
-      <div className={styles.buttonContainer}>
-        <button className={styles.addButton} onClick={() => setIsModalOpen(true)}>Add New Item</button>
-      </div>
-      <div className={styles.gridContainer}>
-        {items.map((item, index) => (
-          <div className={styles.gridItem} key={index}>
-            <img src={item.image} alt={item.name} className={styles.itemImage} />
-            <div className={styles.itemName}>
-              {splitLongName(item.name).split('\n').map((part, index) => (
-                <React.Fragment key={index}>
-                  {part}<br />
-                </React.Fragment>
-              ))}
-            </div>
-            <p className={styles.itemQuantity}>Qty: {item.quantity}</p>
-            <Button variant="contained" color="error" onClick={() => onDelete(item.name)} className={styles.deleteButton}>
-              Delete
-            </Button>
-          </div>
-        ))}
-      </div>
-      <AddItemModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onAdd={(newItem) => {
-          onAddNewItem(newItem);
-          setIsModalOpen(false);
-        }}
-      />
-    </div>
-  );
-}
+	return (
+		<div className={styles.outerContainer}>
+			<div className={styles.boxHeader}>
+				<button
+					className={styles.addButton}
+					onClick={() => setIsModalOpen(true)}
+				>
+					Filter Folders
+				</button>
+				<p>Your Folders</p>
+				<button
+					className={styles.addButton}
+					onClick={() => setIsModalOpen(true)}
+				>
+					<img src="/images/editIcon.png" className={styles.iconImage}/>
+					Edit Folders
+				</button>
+			</div>
+			<div className={styles.gridContainer}>
+				{items.map((item, index) => (
+					<div className={styles.gridItem} key={index}>
+						<img
+							src={item.image}
+							alt={item.name}
+							className={styles.itemImage}
+						/>
+						{/* <Button
+							variant="contained"
+							color="error"
+							onClick={() => onDelete(item.name)}
+							className={styles.deleteButton}
+						>
+							Delete
+						</Button> */}
+						<div className={styles.folderNameOverlay}>
+							<div className={styles.itemName}>
+								<p>{item.name}</p>
+							</div>
+							<p className={styles.itemQuantity}>Qty: {item.quantity}</p>
+						</div>
+					</div>
+				))}
+			</div>
+			<AddItemModal
+				isOpen={isModalOpen}
+				onClose={() => setIsModalOpen(false)}
+				onAdd={(newItem) => {
+					onAddNewItem(newItem);
+					setIsModalOpen(false);
+				}}
+			/>
+		</div>
+	);
+};
 
 export default ItemBox;
