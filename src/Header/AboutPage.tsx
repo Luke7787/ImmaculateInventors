f4c610437b6a94d369814fae011c45702976d16f
Luke7787 <Lukezhuang@yahoo.com>
2024-04-23 15:59:17 -0700

    fixed styling mistakes

--- src/Header/AboutPage.tsx
+++ src/Header/AboutPage.tsx
@@ -13,18 +13,30 @@ const AboutPage = () => {
                     About Us
                 </Typography>
                 <Typography variant="body1" className={styles.bodyText}>
-                    Welcome to My Inventory, your premier source for all things related to inventory management.
-                    We are committed to providing you with the best services possible, focusing on reliability,
-                    customer service, and uniqueness.
+                    <br />
                 </Typography>
                 <Typography variant="body1" className={styles.bodyText}>
-                    Founded in 2023 by the Immaculate Inventors, My Inventory has evolved from its initial setup in a classroom.
-                    United by a shared determination to excel in coding and a passion for helping others become more organized,
-                    we were inspired to launch our business.
+                    Welcome to My Inventory! We’re here to simplify your inventory management with reliable and personalized services
+                    tailored to meet your unique needs. Whether you’re a small business or a large enterprise, we’re excited to help
+                    you find the perfect inventory solutions.
                 </Typography>
                 <Typography variant="body1" className={styles.bodyText}>
-                    We hope you enjoy our service as much as we enjoy offering them to you. If you have any questions or 
+                    <br />
+                </Typography>
+                <Typography variant="body1" className={styles.bodyText}>
+                    Founded in 2023 by the Immaculate Inventors, My Inventory started as a humble classroom project. United by a shared 
+                    passion for coding and organizing, we transformed our initial ideas into a business committed to enhancing inventory 
+                    management solutions.
+                </Typography>
+                <Typography variant="body1" className={styles.bodyText}>
+                    <br />
+                </Typography>
+                <Typography variant="body1" className={styles.bodyText}>
+                    We hope you enjoy our service as much as we enjoy offering them to you. If you have any questions or
                     comments, please don&apos;t hesitate to contact us.
+                </Typography>
+                <Typography variant="body1" className={styles.bodyText}>
+                    <br />
                 </Typography>
                 <Typography variant="h6" component="h2" className={styles.footerText}>
                     Sincerely,
--- src/Header/ContactPage.tsx
+++ src/Header/ContactPage.tsx
@@ -12,9 +12,6 @@ const ContactPage = () => {
                 <Typography variant="h4" component="h1" className={styles.heading}>
                     Get in Touch
                 </Typography>
-                <Typography variant="h4" component="h1" className={styles.heading}>
-                &nbsp;
-                </Typography>
                 <form className={styles.contactForm}>
                     <TextField
                         label={<span style={{fontSize: '20px'}}>Name</span>}
