8ebb9a3b8d45bf85cea342f9ac5db1134e777bef
Luke7787 <Lukezhuang@yahoo.com>
2024-04-23 17:10:53 -0700

    update picture in HomePage

--- src/Header/AboutPage.tsx
+++ src/Header/AboutPage.tsx
@@ -7,13 +7,10 @@ const AboutPage = () => {
     return (
         <div>
             <Header />
-            <img src="/images/RoseGold.jpeg" alt="Decorative Background" className={styles.bgImage} />
+            <img src="/images/maybe.webp" alt="Decorative Background" className={styles.bgImage} />
             <div className={styles.aboutContainer}>
                 <Typography variant="h4" component="h1" className={styles.heading}>
                     About Us
-                </Typography>
-                <Typography variant="body1" className={styles.bodyText}>
-                    <br />
                 </Typography>
                 <Typography variant="body1" className={styles.bodyText}>
                     Welcome to My Inventory! We’re here to simplify your inventory management with reliable and personalized services
@@ -21,22 +18,12 @@ const AboutPage = () => {
                     you find the perfect inventory solutions.
                 </Typography>
                 <Typography variant="body1" className={styles.bodyText}>
-                    <br />
-                </Typography>
-                <Typography variant="body1" className={styles.bodyText}>
                     Founded in 2023 by the Immaculate Inventors, My Inventory started as a humble classroom project. United by a shared 
-                    passion for coding and organizing, we transformed our initial ideas into a business committed to enhancing inventory 
-                    management solutions.
-                </Typography>
-                <Typography variant="body1" className={styles.bodyText}>
-                    <br />
+                    passion for coding and organizing, we transformed our initial ideas into a business committed to enhancing inventory management solutions.
                 </Typography>
                 <Typography variant="body1" className={styles.bodyText}>
                     We hope you enjoy our service as much as we enjoy offering them to you. If you have any questions or
                     comments, please don&apos;t hesitate to contact us.
-                </Typography>
-                <Typography variant="body1" className={styles.bodyText}>
-                    <br />
                 </Typography>
                 <Typography variant="h6" component="h2" className={styles.footerText}>
                     Sincerely,
