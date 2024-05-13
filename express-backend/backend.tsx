34335fe029e1099c16fa3713423cc83a1479ffb8
AnsonWooo <ansonwu020603@gmail.com>
2024-05-12 19:56:25 -0700

    added authenticateToken

--- express-backend/backend.tsx
+++ express-backend/backend.tsx
@@ -128,7 +128,18 @@ app.post('/users/login', async (req: any, res: any) => {
 	} catch {
 		res.status(500).send();
 	}
-})
+});
+
+function authenticateToken(req : any, res : any, next : any) {
+	const authHeader = req.headers['authorization'];
+	const token = authHeader && authHeader.split(' ')[1];
+	if (token == null) return res.sendStatus(401);
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err: any, user: any) => {
+		if (err) return res.sendStatus(403);
+		req.user = user;
+		next();
+	})
+}
 
 
 app.get('/uniqueUser/:username', async (req: any, res: any) => {
